319bd8601cd7abe97cd102ec5d99860259072d44
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-01-15 16:02:08 +0200

    Added draft for `Contact` struct

---
+++ Inc/contact.h
@@ -0,0 +1,15 @@
+//
+// Created by Andrew Yaroshevych on 15.01.2023.
+//
+
+#ifndef PHONE_CONTACT_H
+#define PHONE_CONTACT_H
+
+#include <QString>
+
+struct Contact {
+    QString name;
+    QString number;
+};
+
+#endif //PHONE_CONTACT_H
