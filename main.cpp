1f216812a397b8e8944570abf2ae189836dd6843
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-01-17 16:58:03 +0200

    Added logging

--- main.cpp
+++ main.cpp
@@ -2,13 +2,19 @@
 #include <QtSerialPort/QSerialPort>
 #include <QtSerialPort/QSerialPortInfo>
 
-#include "Inc/serial.h"
-#include "Inc/modem.h"
-#include "Inc/cli.h"
+#include "Inc/modem/serial.h"
+#include "Inc/modem/modem.h"
+#include "Inc/cli/cli.h"
+#include "Inc/logging.h"
 #include <thread>
+
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    auto main_logger = spdlog::basic_logger_mt("main", "../logs/log.txt", true);
+
+    spdlog::flush_on(spdlog::level::debug);
+    spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%^%l%$] [%n] [function %!] [line %#] %v");
 
     // For debugging purposes only
     #ifdef __APPLE__
@@ -28,8 +34,12 @@ int main(int argc, char *argv[]) {
 
     if (!serial.open()) {
         qDebug() << "Serial port was not opened" << serial.errorString();
+        SPDLOG_LOGGER_ERROR(main_logger, "Serial port was not opened: \"{}\"", serial.errorString().toStdString());
         return 1;
+    } else {
+        SPDLOG_LOGGER_INFO(main_logger, "Serial port was opened successfully. Starting modem");
     }
+
 
     Modem modem{serial};
     Screen mainScreen("Main", nullptr);
@@ -79,9 +89,12 @@ int main(int argc, char *argv[]) {
     if (!modemReady) {
         qDebug() << "Modem initialization failed. Exiting...";
         return 1;
+    } else {
+        SPDLOG_LOGGER_INFO(main_logger, "Modem initialization was successful. Preparing CLI");
     }
 
-    std::thread workerThread([&cli] {
+    std::thread workerThread([&cli, &main_logger] {
+        SPDLOG_LOGGER_INFO(main_logger, "CLI listener thread started successfully");
         cli.listen();
     });
 
