1ad4011dc6bb5b8b43090069adc27416cb7fbab3
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-01-23 21:35:37 +0200

    Added `Option` class to use with Screens

---
+++ Src/cli/option.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Andrew Yaroshevych on 23.01.2023.
+//
+
+
+#include <utility>
+
+#include "../../Inc/cli/option.h"
+
+
+Option::Option(QString name, std::function<void()> const& action) : optionName(std::move(name)), action(action) {}
+
+void Option::execute() const {
+    action();
+}
