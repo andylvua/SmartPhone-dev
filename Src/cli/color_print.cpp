675757357b0c478b37eb7cf1a6d211f466a700ae
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-01-23 21:31:07 +0200

    New interface for color printing with `ncurses`

--- Src/cli/color_print.cpp
+++ Src/cli/color_print.cpp
@@ -4,11 +4,28 @@
 
 #include "../../Inc/cli/color_print.h"
 
-void printColored(const char* color, const std::string &text, bool newLine, std::ostream &outStream) {
-    if (newLine) {
-        outStream << color << text << RESET << std::endl;
-    } else {
-        outStream << color << text << RESET;
-        outStream.flush();
+void ColorPrint::initColors() {
+    start_color();
+    init_pair(WHITE, COLOR_WHITE, COLOR_BLACK);
+    init_pair(RED, COLOR_RED, COLOR_BLACK);
+    init_pair(GREEN, COLOR_GREEN, COLOR_BLACK);
+    init_pair(YELLOW, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(FILLED_WHITE, COLOR_BLACK, COLOR_WHITE);
+}
+
+void printColored(int color, const std::string &text, bool newLine, bool bold) {
+    attron(COLOR_PAIR(color));
+    if (bold) {
+        attron(A_BOLD);
     }
+
+    if (newLine) {
+        printw("%s\n", text.c_str());
+    } else {
+        printw("%s", text.c_str());
+        refresh();
+    }
+
+    attroff(COLOR_PAIR(color));
+    attroff(A_BOLD);
 }
