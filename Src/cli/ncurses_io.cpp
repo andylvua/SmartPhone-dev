7fa79cba55e88d2ae41637cbda861846c8695e70
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-01-24 01:56:48 +0200

    Merge pull request #3
    * Reimplemented CLI interface
    
    * Added debug flags
    
    * Updated CLI
    
    * Updated necessary libraries
    
    * New interface for color printing with `ncurses`
    
    * Updated implementation with `ncurses`
    
    * Added DEBUG define to simplify debugging
    
    * Updated I/O to work with `ncurses`
    
    * Added `Option` class to use with Screens
    
    * Added Raspberry-specific definitions
    
    * Added Raspberry-specific compile options
    
    * Minor updated of `Screen` class structure to properly work with optio…
    
    * Fixed missing `<functional>` header
    
    * Updated code structure. Decomposed methods. Fixed some I/O problems
    
    * Renamed from `color_print.h`. Introduced method for reading user input
    
    * Implemented reading method. Renamed from `color_print.h`.
    
    * Naming updates
    
    * Automatic hostname detection for Raspberry Pi
    
    * Minor fix
    
    * Moved linking for `wiringPi` to Raspberry-specific part
    
    * Removed redundant `qDebug()`

---
+++ Src/cli/ncurses_io.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Andrew Yaroshevych on 17.01.2023.
+//
+
+#include "../../Inc/cli/ncurses_io.h"
+
+void ColorPrint::initColors() {
+    start_color();
+    init_pair(WHITE, COLOR_WHITE, COLOR_BLACK);
+    init_pair(RED, COLOR_RED, COLOR_BLACK);
+    init_pair(GREEN, COLOR_GREEN, COLOR_BLACK);
+    init_pair(YELLOW, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(FILLED_WHITE, COLOR_BLACK, COLOR_WHITE);
+}
+
+void printColored(int color, const std::string &text, bool newLine, bool bold) {
+    attron(COLOR_PAIR(color));
+    if (bold) {
+        attron(A_BOLD);
+    }
+
+    if (newLine) {
+        printw("%s\n", text.c_str());
+    } else {
+        printw("%s", text.c_str());
+        refresh();
+    }
+
+    attroff(COLOR_PAIR(color));
+    attroff(A_BOLD);
+}
+
+std::string readString(size_t bufferSize) {
+    echo();
+    char buffer[bufferSize];
+    getstr(buffer);
+    noecho();
+    return {buffer};
+}
