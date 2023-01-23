5c06aef473a9c38f413e24c151a1f73897458f00
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-01-23 21:53:12 +0200

    Minor updated of `Screen` class structure to properly work with options and screen shared ptrs

--- Src/cli/screen.cpp
+++ Src/cli/screen.cpp
@@ -3,17 +3,49 @@
 //
 
 #include <utility>
-
+#include <ncurses.h>
 #include "../../Inc/cli/screen.h"
+#include "../../Inc/cli/color_print.h"
 
-Screen::Screen(QString name, std::shared_ptr<Screen> parentScreen) : parentScreen(std::move(parentScreen)), screenName(std::move(name)) {}
+Screen::Screen(QString name, std::shared_ptr<Screen> parentScreen) : screenName(std::move(name)),
+                                                                     parentScreen(std::move(parentScreen)) {}
 
-void Screen::addScreenOption(const QString &option) {
+void Screen::initScreen() {
+    initscr();
+    raw();
+    keypad(stdscr, TRUE);
+    noecho();
+    curs_set(0);
+
+    ColorPrint::initColors();
+}
+
+void Screen::releaseScreen() {
+    refresh();
+    getch();
+    endwin();
+}
+
+void Screen::addScreenOption(const QString &name, std::function<void()> const& action) {
+    Option option(name, action);
     screenOptions.push_back(option);
 }
 
+int Screen::getActiveOption() const {
+    if (activeOption == -1) {
+        return -1;
+    }
+
+    auto optionsSize = static_cast<int>(screenOptions.size());
+
+    return activeOption % optionsSize;
+}
+
 void Screen::removeScreenOption(const QString &option) {
-    screenOptions.erase(std::remove(screenOptions.begin(), screenOptions.end(), option), screenOptions.end());
+    screenOptions.erase(std::remove_if(screenOptions.begin(), screenOptions.end(),
+                                       [&option](const Option &screenOption) {
+                                           return screenOption.optionName == option;
+                                       }), screenOptions.end());
 }
 
 void Screen::addNotification(const QString &notification) {
@@ -26,4 +58,8 @@ void Screen::removeNotification(const QString &notification) {
 
 void Screen::removeScreenOption(int index) {
     screenOptions.erase(screenOptions.begin() + index);
+}
+
+void Screen::addChildScreen(const std::shared_ptr<Screen> &screen) {
+    childScreens.push_back(screen);
 }
