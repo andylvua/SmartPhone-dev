062ba339c78ee57c617b2d6dca1daa798dc2fb22
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-01-26 15:27:24 +0200

    Moved all color definitions to the dedicated header

---
+++ Inc/cli/colors.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Andrew Yaroshevych on 25.01.2023.
+//
+
+#ifndef PHONE_COLORS_HPP
+#define PHONE_COLORS_HPP
+
+constexpr const char* RESET = "\033[0m";
+constexpr const char* RED_COLOR = "\033[31m";
+constexpr const char* GREEN_COLOR = "\033[32m";
+constexpr const char* YELLOW_COLOR = "\033[33m";
+constexpr const char* WHITE_COLOR = "\033[37m";
+
+constexpr int WHITE_PAIR = 1;
+constexpr int RED_PAIR = 2;
+constexpr int GREEN_PAIR = 3;
+constexpr int YELLOW_PAIR = 4;
+constexpr int FILLED_WHITE_PAIR = 5;
+
+#endif //PHONE_COLORS_HPP
