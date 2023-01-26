c1637e11652c8603ce2245ac64319cbe86e63f94
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-01-26 15:29:45 +0200

    Separated CLI macros

---
+++ Inc/cli/cli_macros.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Andrew Yaroshevych on 26.01.2023.
+//
+
+#ifndef PHONE_CLI_MACROS_HPP
+#define PHONE_CLI_MACROS_HPP
+
+#define SCREEN_SHARED_PTR(name, parentScreen) std::make_shared<Screen>(name, parentScreen)
+
+#define GO_BACK [&](){gotoParentScreen();}
+#define CHANGE_SCREEN(screenName) [&](){changeScreen(screenName);}
+#define EXECUTE_METHOD(method) [&](){method();}
+
+#endif //PHONE_CLI_MACROS_HPP
