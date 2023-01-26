8f69488548c08f15aa5374b5eed2b12312052a0c
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-01-26 15:30:28 +0200

    Minor improvements. Fixed I/O bugs and updated file types

--- Src/decoder.cpp
+++ Src/decoder.cpp
@@ -1,13 +1,13 @@
+#include "../Inc/decoder.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
 #include <bitset>
-#include <QString>
-#include "../Inc/decoder.h"
 #include <codecvt>
 #include <locale>
+#include <QString>
 
-const std::string Decoder::_hexChartoBin(char c)
+std::string Decoder::hexCharToBin(char c)
 {
     switch(toupper(c))
     {
@@ -27,75 +27,72 @@ const std::string Decoder::_hexChartoBin(char c)
         case 'D': return "1101";
         case 'E': return "1110";
         case 'F': return "1111";
+        default: return "0000";
     }
 }
 
-const std::vector<std::string> Decoder::_hexStrtoBinStr(const std::string &hex)
-{
+std::vector<std::string> Decoder::hexStrToBinStr(const std::string &hex) {
     std::vector<std::string> bin;
-    for(unsigned i = 0; i != hex.length()/2; ++i)
-        bin.push_back(_hexChartoBin(hex[2*i]) + _hexChartoBin(hex[2*i+1]));
+    for (unsigned i = 0; i != hex.length() / 2; ++i)
+        bin.push_back(hexCharToBin(hex[2 * i]) + hexCharToBin(hex[2 * i + 1]));
     return bin;
 }
 
-QString Decoder::decode7Bit(const QString& encoded){
+QString Decoder::decode7Bit(const QString &encoded) {
     QString decoded = "";
     std::string message = encoded.toStdString();
-    std::cin>> message;
-    // CE3048 -> 11001110 00111000 01001000
-    std::vector<std::string> message_in_bin = _hexStrtoBinStr(message);
-    //decode message using 7-bit ASCII
-    // 11001110 00111000 01001000 -> 1001110 1110001 0100000
-    std::vector <std::string> decoded_message;
-    std::string temp = "";
-    for (int i=0; i < message_in_bin.size(); i++){
-        if (temp.size() == 7){
-            decoded_message.push_back(temp);
+    std::cin >> message;
+
+    std::vector<std::string> messageInBin = hexStrToBinStr(message);
+    std::vector<std::string> decodedMessage;
+    std::string temp;
+
+    for (size_t i = 0; i < messageInBin.size(); i++) {
+        if (temp.size() == 7) {
+            decodedMessage.push_back(temp);
             temp = "";
         }
-        decoded_message.push_back(message_in_bin[i].substr((i)%7+1)+temp);
-        temp = message_in_bin[i].substr(0,i%7+1);
-
+        decodedMessage.push_back(messageInBin[i].substr((i) % 7 + 1) + temp);
+        temp = messageInBin[i].substr(0, i % 7 + 1);
     }
-    for (int i = 0; i < decoded_message.size(); i++){
-        std::bitset<7> b(decoded_message[i]);
+
+    for (const auto & i : decodedMessage) {
+        std::bitset<7> b(i);
         decoded += char(b.to_ulong());
     }
+
     decoded += "\n";
     return decoded;
 }
-QString Decoder::decodeUcs2(const QString& encoded){
-        std::string hexString = encoded.toStdString();
-        std::string unicodeLiteral;
-        for (int i = 0; i < hexString.length(); i += 4) {
-            std::string hex = hexString.substr(i, 4);
-            unicodeLiteral += "\\u" + hex;
+
+QString Decoder::decodeUCS2(const QString &encoded) {
+    std::string hexString = encoded.toStdString();
+    std::string unicodeLiteral;
+    for (size_t i = 0; i < hexString.length(); i += 4) {
+        std::string hex = hexString.substr(i, 4);
+        unicodeLiteral += "\\u" + hex;
+    }
+
+    std::wstring ws;
+    ws.reserve(unicodeLiteral.size());
+
+    for (size_t i = 0; i < unicodeLiteral.size();) {
+        char ch = unicodeLiteral[i];
+        i++;
+
+        if ((ch == '\\') && (i < unicodeLiteral.size()) && (unicodeLiteral[i] == 'u')) {
+            auto wc = static_cast<wchar_t>(std::stoi(unicodeLiteral.substr(i + 1, 4),
+                                                     nullptr,
+                                                     16));
+            i += 5;
+            ws.push_back(wc);
+        } else {
+            ws.push_back(static_cast<wchar_t>(ch));
         }
+    }
 
-        std::wstring ws;
-        ws.reserve(unicodeLiteral.size());
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+    std::string s = conv.to_bytes(ws);
 
-        for(size_t i = 0; i < unicodeLiteral.size();)
-        {
-            char ch = unicodeLiteral[i];
-            i++;
-
-            if ((ch == '\\') && (i < unicodeLiteral.size()) && (unicodeLiteral[i] == 'u'))
-            {
-                auto wc = static_cast<wchar_t>(std::stoi(unicodeLiteral.substr(i + 1, 4),
-                                                         nullptr,
-                                                         16));
-                i += 5;
-                ws.push_back(wc);
-            }
-            else
-            {
-                ws.push_back(static_cast<wchar_t>(ch));
-            }
-        }
-
-        std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
-        std::string s = conv.to_bytes(ws);
-
-        return QString::fromStdString(s);
+    return QString::fromStdString(s);
 }
